f4f61ea25f3e80b1b23cb19cc3100f8c0efa9a05
Sahil Yogi <sahilyogi@Sahils-MacBook-Air.local>
2026-07-22 23:55:13 +0530

    Clean up old legacy files (old_index, old_styles, old_script)

--- backend/test-server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/api/test', (req, res) => res.send('hello'));
-app.listen(8999, () => console.log('test server running'));
-setInterval(() => {}, 1000);
--- backend/test-server2.js
+++
@@ -1,3 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => res.end('ok'));
-server.listen(8999, () => console.log('native server running'));
